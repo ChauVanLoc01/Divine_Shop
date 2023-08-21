8594fc46568dd49b3791bb349287419bfc2116f5
ChauVanLoc <19130121@st.hcmuaf.edu.vn>
2023-08-21 10:36:25 +0700

    update

--- divine_shop/src/constants/base-url.constant.ts
+++ divine_shop/src/constants/base-url.constant.ts
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://ec2-52-77-254-68.ap-southeast-1.compute.amazonaws.com:1234/'
+export const baseUrl = 'http://ec2-52-77-254-68.ap-southeast-1.compute.amazonaws.com:443/'
